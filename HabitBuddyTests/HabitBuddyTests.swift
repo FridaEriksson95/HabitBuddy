fe242b4c9d8b247e6ff4a7b113a462e437cdddca
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-04-29 00:07:11 +0200

    Initial Commit

---
+++ HabitBuddyTests/HabitBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  HabitBuddyTests.swift
+//  HabitBuddyTests
+//
+//  Created by Frida Eriksson on 2025-04-29.
+//
+
+import Testing
+@testable import HabitBuddy
+
+struct HabitBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
