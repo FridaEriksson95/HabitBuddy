24c9f254a0b32f34f3f820409927c9230271ca74
FridaEriksson95 <fridaeriiksson@gmail.com>
2025-05-05 16:20:34 +0200

    funktionell start med app där jag gjort design, logga, färger, fixat så man har dagsvy för att kunna markera sina vanor till "klara", klicka sig in på sin vana för att se hur många veckor man utfört dessa och antal dagar. Kunna lägga till ny vana med symboler och kunna ändra sin vana med bild och titel. Nästa steg, strukturera upp kod, få till mer funktioner som att se dagar bakåt och när man utförde osv, skriva anteckning på nån specifik händelse etc.

---
+++ Habit+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Habit+CoreDataClass.swift
+//  HabitBuddy
+//
+//  Created by Frida Eriksson on 2025-04-29.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(HabitEntity)
+public class HabitEntity: NSManagedObject {
+
+}
---
+++ Habit+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  Habit+CoreDataProperties.swift
+//  HabitBuddy
+//
+//  Created by Frida Eriksson on 2025-04-29.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension HabitEntity {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<HabitEntity> {
+        return NSFetchRequest<HabitEntity>(entityName: "HabitEntity")
+    }
+
+    @NSManaged public var lastCompletedDate: Date?
+    @NSManaged public var isCompletedToday: Bool
+    @NSManaged public var title: String?
+    @NSManaged public var streak: Int16
+    @NSManaged public var symbolName: String?
+}
+
+extension HabitEntity : Identifiable {
+
+}
